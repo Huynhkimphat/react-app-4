d110296b3aa6550f30793e5d862de9ef6d97f036
Huynh Kim Phat <hkimphat2012@gmail.com>
2021-05-27 22:59:16 +0700

    Update project

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import React, { useState, useEffect } from "react";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
+// TODO : useEffect
+// * useEffect(()=>{}) --> run when any components reload or re-render
+// * useEffect(()=>{},[]) --> run 1 time when page reload
+// * useEffect(()=>{},[x,y]) --> run when component x,y reload
+// * useEffect(()=>{...return ()=>{}},...) -> return statement not run when page reload<the first time>)
 
 function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    // No dependencies => just run when load page
     useEffect(() => {
         const storedUserLoggedInInformation =
             localStorage.getItem("isLoggedIn");
-        if (storedUserLoggedInInformation === 1) {
-            console.log("Hi");
+        if (storedUserLoggedInInformation === "1") {
             setIsLoggedIn(true);
         }
     }, []);
@@ -22,6 +27,7 @@ function App() {
     };
 
     const logoutHandler = () => {
+        localStorage.removeItem("isLoggedIn");
         setIsLoggedIn(false);
     };
 
